c6cab731241e95e1ac068a87a6e1c5b017cea4e0
phdmhenrique <pablo.monteirorgt19@gmail.com>
2026-03-07 16:20:39 -0300

    chore: initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
