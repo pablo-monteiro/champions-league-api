34e7c5cae84ecfe768f683fdf8aa7beec5380edf
phdmhenrique <pablo.monteirorgt19@gmail.com>
2026-03-11 09:41:59 -0300

    feat: adding cors on project

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes";
 
 function createApp() {
   const app = express();
   app.use(express.json());
+
+  // const corsOptions = {
+  //   origin: "http://localhost:3000",
+  //   methods: ["GET"],
+  //   credentials: true,
+  // };
+
+  app.use(cors());
   app.use("/api", routes);
 
   return app;
